8cee84ee1e4c9de3fe1d80e6e0f58eab3fa4d032
Gołąb Sebastian <sebastian.golab@k5054macbook.local>
2025-01-28 18:29:51 +0100

    layout update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={roboto.className}>test {children}</body>
+      <body className={roboto.className}>{children}</body>
     </html>
   );
 }
