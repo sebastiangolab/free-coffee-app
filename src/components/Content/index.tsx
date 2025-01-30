503252517f207ac59deb2ead742c9c9bc7bddec5
Gołąb Sebastian <sebastian.golab@k5054macbook.local>
2025-01-30 17:47:05 +0100

    Create Content component and update global styles

---
+++ src/components/Content/index.tsx
@@ -0,0 +1,32 @@
+import React, { ReactElement } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import CoffeeCupImage from "@/images/coffee-cup.png";
+import styles from "./content.module.css";
+
+const Content = (): ReactElement => {
+  return (
+    <div className={styles.content}>
+      <h1 className={styles.title}>Free Coffee</h1>
+      <h2 className={styles.subtitle}>for me, only 1 euro</h2>
+
+      <Image
+        className={styles.image}
+        src={CoffeeCupImage}
+        alt={"coffee cup"}
+        width={384}
+        height={384}
+      />
+
+      <Link
+        className={styles.authorText}
+        href="https://sebastiangolab.pl/"
+        title="Sebastian Golab page"
+      >
+        By Sebastian Golab
+      </Link>
+    </div>
+  );
+};
+
+export default Content;
