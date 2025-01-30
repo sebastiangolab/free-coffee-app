7da50951c5d15bedf7f355a42fcf5b97ade7c8d1
Gołąb Sebastian <sebastian.golab@k5054macbook.local>
2025-01-30 23:55:42 +0100

    Update CheckoutForm component

--- src/components/CheckoutForm/index.tsx
+++ src/components/CheckoutForm/index.tsx
@@ -49,7 +49,7 @@ const CheckoutForm = ({
     const { error } = await stripe.confirmPayment({
       elements,
       confirmParams: {
-        return_url: `/payment-success?payment_intent={CHECKOUT_SESSION_ID}`,
+        return_url: `${process.env.BASE_URL}payment-success?payment_intent={CHECKOUT_SESSION_ID}`,
       },
     });
 
