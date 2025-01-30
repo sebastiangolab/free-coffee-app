cf0852e967c827f1fb731ab4482e2a843c62d1f4
Gołąb Sebastian <sebastian.golab@k5054macbook.local>
2025-01-30 19:40:01 +0100

    Update front page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,50 +1,13 @@
-"use client";
-
-import { useEffect, useState } from "react";
-import { NextResponse } from "next/server";
-import CheckoutForm from "@/components/CheckoutForm";
-import { staticAmountCents } from "@/config";
-import { Elements } from "@stripe/react-stripe-js";
-import {
-  loadStripe,
-  StripeElementsOptionsClientSecret,
-} from "@stripe/stripe-js";
-
-if (!process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY) {
-  throw Error("NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY is not defined");
-}
-
-const stripePromise = loadStripe(
-  process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
-  { locale: "en" }
-);
+import Container from "@/components/Container";
+import Content from "@/components/Content";
+import PaymentCheckout from "@/components/PaymentCheckout";
 
 export default function HomePage() {
-  const [clientSecret, setClientSecret] = useState<string>();
-
-  useEffect(() => {
-    fetch("/api/create-payment-intent", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ amount: staticAmountCents }),
-    })
-      .then((res) => res.json())
-      .then((data) => setClientSecret(data.clientSecret));
-  }, []);
-
-  const options: StripeElementsOptionsClientSecret = {
-    clientSecret: clientSecret,
-  };
-
-  if (!clientSecret) {
-    return <h1>isLoading...</h1>;
-  }
-
   return (
-    <div>
-      <Elements options={options} stripe={stripePromise}>
-        <CheckoutForm />
-      </Elements>
-    </div>
+    <Container>
+      <Content />
+
+      <PaymentCheckout />
+    </Container>
   );
 }
